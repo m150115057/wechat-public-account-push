8fb72c46f74b4eeac76bfbe1a9c924d9994af263
m150115057 <113873989+m150115057@users.noreply.github.com>
2022-09-19 14:28:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "5f5c2c4aec6820ba3ff0d2640c6047cb",
+  APP_SECRET: "1d982390abb50396ed2a8eabf9c68680",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
